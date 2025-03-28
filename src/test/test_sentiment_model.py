2061916a5b462fd0fb32bf206dbf208beb419e76
kevin.ta <kevin.ta@emeria.eu>
2025-03-28 18:12:49 +0100

    done

--- src/test/test_sentiment_model.py
+++ src/test/test_sentiment_model.py
@@ -7,11 +7,11 @@ import os
 
 # Ajouter le répertoire src au chemin de recherche des modules
 current_dir = os.path.dirname(os.path.abspath(__file__))
-src_dir = os.path.join(current_dir, "src")
+src_dir = os.path.join(current_dir, "src/test")
 sys.path.insert(0, src_dir)
 
 # Importer le module de test de sentiment
-from src.test.test_sentiment import main
+from test_sentiment import main
 
 if __name__ == "__main__":
     main() 
